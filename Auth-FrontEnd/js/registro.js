9cac0dd25239da1d15340ede0a91dcc797493088
Jottinha <joaovitorfps852@gmail.com>
2023-06-10 21:46:48 -0300

    Criando paginas de registro

---
+++ Auth-FrontEnd/js/registro.js
@@ -0,0 +1,31 @@
+function verificaRegistro(){
+    if (verificaEmail()){
+        if (verificaSenha()){
+            document.querySelector("p#mensagem_erro").innerHTML = "";
+            //chama dunção para mandar para o back o email e a senha  
+        }else{
+            document.querySelector("p#mensagem_erro").innerHTML = "Senhas não conferem"
+        }
+    }else{
+        document.querySelector("p#mensagem_erro").innerHTML = "Email invalido"
+    }
+}
+
+function verificaSenha(){
+    var primeiraSenha = document.querySelector("input#Password_input").value;
+    var segundaSenha = document.querySelector("input#Confirm_password_input").value;
+    return (primeiraSenha === segundaSenha) ? true : false;
+}
+
+function verificaEmail(){
+    var email = document.querySelector("input#email_input").value;
+    return eValidoEmail(email);
+    //condição e metodo que busca email no back
+
+}
+
+function eValidoEmail(email){
+    var regex = /^[\w.-]+@[a-zA-Z_-]+(?:\.[a-zA-Z]+)+$/;
+    var eValido = regex.test(email);
+    return (eValido) ? true : false;
+}
